b90e935c1ae60923d48e370226f9cb88d46febd1
fabzum <60756419+fabzum@users.noreply.github.com>
2020-03-18 14:45:15 -0700

    importing csv

---
+++ **PyBank**/main.py
@@ -0,0 +1,10 @@
+##PyBank Challenge
+
+#Import Module to create file paths across operating systems
+import os
+
+#Import Module for reading CSV files
+import csv
+
+#Provide the path for the CSV file that should be read
+cvspath = os.path.join("budget_data.csv")
