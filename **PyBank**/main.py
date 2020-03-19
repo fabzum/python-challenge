4a56ca65af3d791fea227ea34ff3509eae7fd572
fabzum <60756419+fabzum@users.noreply.github.com>
2020-03-18 17:34:22 -0700

    CSV reader

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -7,4 +7,17 @@ import os
 import csv
 
 #Provide the path for the CSV file that should be read
-cvspath = os.path.join("budget_data.csv")
+csvpath = os.path.join("budget_data.csv")
+
+#Read CSV
+with open(csvpath) as csvfile:
+     csvreader = csv.reader(csvfile, delimiter=",")
+ 
+     print(csvreader)
+
+     csv_header = next(csvreader)
+     print(csv_header)
+
+     for row in csvreader:
+         print(row)
+
