6b379d245c864f95205f53a259876fe21f19010a
fabzum <60756419+fabzum@users.noreply.github.com>
2020-03-20 14:55:56 -0700

    Pybank update

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -14,19 +14,56 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     print(csvreader)
 
+    #Skip Header
     csv_header = next(csvreader)
     print(csv_header)
 
+    #Set count_row and sum_month to 0
     count_row = 0
-    sum_row = 0
-     
+    net_total = 0
+    pre_amount = 0
+    sum_change_list = 0
+
+    list_of_months =[]
+    list_of_profits =[]
+    change_list =[]
+    
+    
+    #Loop through rows
     for row in csvreader:
         count_row+=1
-        sum_row+=int(row[1])
-        print(row)
+        list_of_months.append(row[0])
+
+        net_total+=int(row[1])
+        list_of_profits.append(row[1])
+
+        if count_row == 1:
+            pre_amount = int(row[1])
+        elif count_row > 1:
+            change = int(row[1]) - pre_amount
+            change_list.append(change)
+            pre_amount = int(row[1])
+     
+        
     
+    
+    avergage_change_list=sum(change_list)/len(change_list)
+    greatest_increase=max(change_list)
+    greatest_decrease=min(change_list)
+    greatest_increase_index=change_list.index(greatest_increase)
+    greatest_decrease_index=change_list.index(greatest_decrease)
+    
+    greatest_increase_month=list_of_months[int(greatest_increase_index) + 1]
+    greatest_decrease_month=list_of_months[int(greatest_decrease_index) + 1]
+
     print(count_row)
-    print(sum_row)
+    print(net_total)
+    print(avergage_change_list)
+    print(str(greatest_increase) + str(greatest_increase_month))    
+    print(str(greatest_decrease) + str(greatest_decrease_month)) 
+    
+    
+   
      
     
      
