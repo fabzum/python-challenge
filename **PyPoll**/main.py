31d41796693231e5eb8ca2385abb039ba68da764
fabzum <60756419+fabzum@users.noreply.github.com>
2020-03-21 11:10:44 -0700

    Done

--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -54,7 +54,7 @@ with open(csvpath) as csvfile:
     
     #Calculates the percentage and appends it to the percentage_of_vote_list
     for vote in number_of_votes:
-        percentage_of_vote.append(round(vote/total_of_votes*100, 3))
+        percentage_of_vote.append(round(vote/total_of_votes*100, 3)) #Why does it not round to 3 decimal points?
     
     #Zipps the previous derived 3 lists into one (summary_data)
     summary_data = list(zip(final_list_of_candidates, percentage_of_vote, number_of_votes))
@@ -64,6 +64,8 @@ with open(csvpath) as csvfile:
         if max(number_of_votes) == candidate[2]:
             winner.append(candidate[0])
 
+    #Printing out the Results
+    #-----------------------------
     #print(list_of_candidates)
     print(total_of_votes)
     #print(number_of_votes)
@@ -72,6 +74,19 @@ with open(csvpath) as csvfile:
     print(summary_data)
     print(winner)
 
-    
+
+#Create the output path for the text file (Textfile has been created via touch output.txt command in terminal)
+output_path = os.path.join("output.txt")
+with open(output_path, "w") as writefile:
+    #Write down the solutions in the textfile
+    writefile.writelines("Election Results\n")
+    writefile.writelines("------------------" + "\n")
+    writefile.writelines("Total Votes:" + str(total_of_votes) + "\n")
+    writefile.writelines("------------------" + "\n")
+    for i in range(len(final_list_of_candidates)):
+        writefile.writelines(f"{final_list_of_candidates[i]}: {percentage_of_vote[i]}% ({number_of_votes[i]})\n")
+    writefile.writelines("------------------" + "\n")
+    writefile.writelines(f"Winner: {final_list_of_candidates[0]}\n")
+
 
 
