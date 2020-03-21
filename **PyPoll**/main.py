b7dc6c473d4a364a2088fc116f116b392f2956c0
fabzum <60756419+fabzum@users.noreply.github.com>
2020-03-21 07:50:12 -0700

    until summary_data

--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -19,38 +19,51 @@ with open(csvpath) as csvfile:
     
     #print(csv_header)
 
-    #Set count_row, net_total, pre_amount and sum_change_list to 0
-    count_row = 0
-    candidates = 0
-    #pre_amount = 0 (not needed to be set at 0, why though?)
+
   
-    #Create Lists to be filled in the Loop
+    #Create Lists and a Dictionnary to be filled in the Loop
     list_of_candidates =[]
     final_list_of_candidates = []
     number_of_votes = []
     poll ={}
+    percentage_of_vote = []
+    #Put number of votes to zero
     total_of_votes = 0
     
-    #Loop through rows
+    #Loop through rows in CSV
     for row in csvreader:
         
-        #Count the number of rows, which gives us the total number of months in the dataset and append them in the list_of_months
+        #Count the number of rows, which gives us the total number of votes in the dataset
         total_of_votes+=1
         
-        #candidates+=int(row[2])
+        #Create a Full list with Candidates (includes dublicates)
         list_of_candidates.append(row[2])
 
+        #Defines the candidates in row[2] as keys to the dictionary  
         if row [2] in poll.keys():
             poll[row[2]] = poll[row[2]]+1
 
         else:
             poll[row[2]] = 1
-
+    
+    #Returns two lists, once for the key (candidates) and one for its value (the number of votes)
     for key, value in poll.items():
         final_list_of_candidates.append(key)
         number_of_votes.append(value)
+    
+    #Calculates the percentage and appends it to the percentage_of_vote_list
+    for vote in number_of_votes:
+        percentage_of_vote.append(round(vote/total_of_votes*100, 3))
+    
+    #Zipps the previous derived 3 lists into one (summary_data)
+    summary_data = list(zip(final_list_of_candidates, percentage_of_vote, number_of_votes))
 
-    print(total_of_votes)
-    print(number_of_votes)
-    print(final_list_of_candidates)
     #print(list_of_candidates)
+    print(total_of_votes)
+    #print(number_of_votes)
+    #print(final_list_of_candidates)
+    #print(percentage_of_vote)
+    print(summary_data)
+
+    
+
